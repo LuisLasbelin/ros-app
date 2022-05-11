70bbd9a22be40c785651fef062c13af6f8aa7d5f
Luis Belloch <luis.bellochmartinez@gmail.com>
2022-05-11 18:30:56 +0200

    H11-T4-envio de mensajes por roslib

--- web-app/test/firebase.test.js
+++
@@ -1,7 +0,0 @@
-// ERROR: da un error en import(constants)
-const putData =  require("../src/ros.js");
-
-test("Enviar un dato a Firebase", async () => {
-    const result = putData("test", {test: "cositas"});
-    expect(result.test).toBe("cositas");
-});
